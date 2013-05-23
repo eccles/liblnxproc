e56215162116020caa61c009bebe131e5a4369a7
Paul Hewlett <phewlett76@gmail.com>
2013-05-23 10:36:17 +0100

    Add logging to check.sh

--- check.sh
+++ check.sh
@@ -15,20 +15,20 @@ fi
 INDOPTS="-kr -nut -l80 -nce -bad -bap -sob -psl"
 INDTYPES="-T LNXPROC_ARRAY_T -T LNXPROC_ARRAY_ITERATE_FUNC -T LNXPROC_BASE_T -T LNXPROC_BASE_METHOD -T LNXPROC_ERROR_T -T LNXPROC_ERROR_CALLBACK"
 
-AST=$( type astyle )
-if [ $? -ne 0 ]
-then
-	echo "astyle is not accessible (maybe not installed)"
-    exit 1
-fi
-ASTOPTS="--style=1tbs --break-blocks=all --break-closing-brackets --align-pointer=middle"
-ASTTYPES="-T LNXPROC_ARRAY_T -T LNXPROC_ARRAY_ITERATE_FUNC -T LNXPROC_BASE_T -T LNXPROC_BASE_METHOD -T LNXPROC_ERROR_T -T LNXPROC_ERROR_CALLBACK"
+#AST=$( type astyle )
+#if [ $? -ne 0 ]
+#then
+#	echo "astyle is not accessible (maybe not installed)"
+#    exit 1
+#fi
+#ASTOPTS="--style=1tbs --break-blocks=all --break-closing-brackets --align-pointer=middle"
+#ASTTYPES="-T LNXPROC_ARRAY_T -T LNXPROC_ARRAY_ITERATE_FUNC -T LNXPROC_BASE_T -T LNXPROC_BASE_METHOD -T LNXPROC_ERROR_T -T LNXPROC_ERROR_CALLBACK"
 
-if [ $# -lt 1 ]
-then
-	echo "You must specify C filename(s) as first argument"
-	exit 1
-fi
+#if [ $# -lt 1 ]
+#then
+#	echo "You must specify C filename(s) as first argument"
+#	exit 1
+#fi
 #
 #echo ":$*:"
 code=0
@@ -45,6 +45,7 @@ do
     then
         for j in $i/*
         do
+            echo "Indent $j ..."
             indent ${INDOPTS} ${INDTYPES} $j
 #           astyle ${ASTOPTS} $i
             if [ $? -ne 0 ]
@@ -53,6 +54,7 @@ do
             fi
         done
     else
+        echo "Indent $i ..."
         indent ${INDOPTS} ${INDTYPES} $i
 #       astyle ${ASTOPTS} $i
         if [ $? -ne 0 ]
