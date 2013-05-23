9000068e780f64835dc07b615f34f237a76bf45f
Paul Hewlett <phewlett76@gmail.com>
2013-05-23 10:35:43 +0100

    Comment uthash debug script

--- src/expand_results.sh
+++ src/expand_results.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+#
+# used to test the uthash.h hash functions
+#
 gcc -DDEBUG=1 -E -I../src results.c > results.hash
 egrep -v '^#' results.hash > results.trim
 ../check.sh results.trim 
